e50a70f8be25f6717e45b551f6c5911903682fbd
JulianLovell <jlovell108@gmail.com>
2024-08-12 12:49:18 +0930

    fixed

--- week2/AlternateColors/AlternateColors.hpp
+++ week2/AlternateColors/AlternateColors.hpp
@@ -5,12 +5,12 @@ class AlternateColors {
 public:
     string getColor(long r, long g, long b, long k) {
         // If kth ball is red then return RED
-        if (k <= r) {
+        if (k <= r+1) {
             return "RED";
         }
         
         // If kth ball is green then return green
-        if (k <= r + g) {
+        if (k <= r + g+1) {
             return "GREEN";
         }
         
