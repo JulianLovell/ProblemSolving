ce854a7a8278b0f75eefc37feb7659c8630398d0
JulianLovell <jlovell108@gmail.com>
2024-08-14 14:25:42 +0930

    Arrows: Fixed initial length value and arrow shaft search

--- week3/Arrows/Arrows.hpp
+++ week3/Arrows/Arrows.hpp
@@ -6,66 +6,41 @@ using namespace std;
 class Arrows {
 public:
     int longestArrow(string s) {
-        // Set initial value to -1 so that if none are found then this is returned
         int longestFound = -1;
 
         // Iterate over the string to find potential arrows
         for (int i = 0; i < s.length(); i++) {
-            int length = 1; // Start counting length from the first character
+            // Start counting length from the first character
+            int length = 1;
 
-            // Check for left single arrow "<-"
+            // Check for left single arrow "<-" or "<="
             if (s[i] == '<') {
                 int temp = i + 1;
-                while (temp < s.length() && s[temp] == '-') {
+                while (temp < s.length() && (s[temp] == '-' || s[temp] == '=')) {
                     length++;
                     temp++;
                 }
-                if (length > 1) {
-                    longestFound = max(longestFound, length);
-                }
+                longestFound = max(longestFound, length);
             }
 
-            // Reset length and then check for left double arrow "<="
-            length = 1;
-            if (s[i] == '<') {
-                int temp = i + 1;
-                while (temp < s.length() && s[temp] == '=') {
-                    length++;
-                    temp++;
-                }
-                if (length > 1) {
-                    longestFound = max(longestFound, length);
-                }
-            }
-
-            // Check for right single arrow "->"
-            if (s[i] == '-') {
-                // Length should start at 0 for "-" and "="
+            // Check for right single arrow "->" or "=>"
+            if (s[i] == '-' || s[i] == '=') {
                 length = 0;
                 int temp = i;
-                while (temp < s.length() && s[temp] == '-') {
+                while (temp < s.length() && (s[temp] == '-' || s[temp] == '=')) {
                     length++;
                     temp++;
                 }
                 if (temp < s.length() && s[temp] == '>') {
-                    // Then include the '>'
+                    // Include arrow head
                     length++;
                     longestFound = max(longestFound, length);
                 }
             }
 
-            // Check for right double arrow "=>"
-            if (s[i] == '=') {
-                length = 0;
-                int temp = i;
-                while (temp < s.length() && s[temp] == '=') {
-                    length++;
-                    temp++;
-                }
-                if (temp < s.length() && s[temp] == '>') {
-                    length++;
-                    longestFound = max(longestFound, length);
-                }
+            // Check for just ">" or "<"
+            if (s[i] == '<' || s[i] == '>') {
+                longestFound = max(longestFound, 1);
             }
         }
 
