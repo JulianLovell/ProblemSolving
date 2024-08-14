feb84919397617b5ffa75042e4a0b3b37c0ae084
JulianLovell <jlovell108@gmail.com>
2024-08-14 12:56:16 +0930

    Arrows: Initial implementation

--- week3/Arrows.hpp
+++ week3/Arrows.hpp
@@ -1,3 +1,73 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+class Arrows {
+public:
+    int longestArrow(string s) {
+        int longestFound = -1;
+
+        // Iterate over the string to find potential arrows
+        for (int i = 0; i < s.length(); i++) {
+            int length = 1; // Start counting length from the first character
+
+            // Check for left single arrow "<-"
+            if (s[i] == '<') {
+                int temp = i + 1;
+                while (temp < s.length() && s[temp] == '-') {
+                    length++;
+                    temp++;
+                }
+                if (length > 1) {
+                    longestFound = max(longestFound, length);
+                }
+            }
+
+            // Reset length and then check for left double arrow "<="
+            length = 1;
+            if (s[i] == '<') {
+                int temp = i + 1;
+                while (temp < s.length() && s[temp] == '=') {
+                    length++;
+                    temp++;
+                }
+                if (length > 1) {
+                    longestFound = max(longestFound, length);
+                }
+            }
+
+            // Check for right single arrow "->"
+            if (s[i] == '-') {
+                // Length should start at 0 for "-" and "="
+                length = 0;
+                int temp = i;
+                while (temp < s.length() && s[temp] == '-') {
+                    length++;
+                    temp++;
+                }
+                if (temp < s.length() && s[temp] == '>') {
+                    length++; // Include the '>'
+                    longestFound = max(longestFound, length);
+                }
+            }
+
+            // Check for right double arrow "=>"
+            if (s[i] == '=') {
+                length = 0;
+                int temp = i;
+                while (temp < s.length() && s[temp] == '=') {
+                    length++;
+                    temp++;
+                }
+                if (temp < s.length() && s[temp] == '>') {
+                    // Then include the ">"
+                    length++;
+                    longestFound = max(longestFound, length);
+                }
+            }
+        }
+
+        return longestFound;
+    }
+};
