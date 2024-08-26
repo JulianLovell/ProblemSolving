6ef5a170fe6d681464f31926ff9f964efc8d9a0a
JulianLovell <jlovell108@gmail.com>
2024-08-26 14:42:34 +0930

    Run Length Encoding: First Attemept

--- week5/RunLengthEncoding/RunLengthEncoding.hpp
+++ week5/RunLengthEncoding/RunLengthEncoding.hpp
@@ -3,36 +3,44 @@
 
 using namespace std;
 
-class RunLengthEncoding
-{
+class RunLengthEncoding {
 public:
-    string decode(string text)
-    {
+    string decode(string text) {
         // Initialise new empty string
-        string newString = {};
+        string newString = "";
 
-        // Initialise multiplyer
-        int multi = 1;
+        int n = text.size();
+        int i = 0;
 
-        for (int i = 0; i < text.size(); ++i)
-        {
-            // Update multiplyer if element is number
-            if (isdigit(text[i]) == true)
-            {
-                multi = stoi(text[i]);
+        while (i < n) {
+            int multi = 0;
+
+            // Check if the current character is a digit
+            if (isdigit(text[i])) {
+                // Concert char to int
+                multi = text[i] - '0';
+                ++i;
+            } else {
+                // If no number is found, default multiplier to 1
+                multi = 1;
             }
-            else
-            {
-                // pushback text[i] 'multi' times, default is 1 if no new number found
-                for (int j = 0; j < multi; ++j)
-                {
+
+            // Append the character 'multi' times
+            if (i < n && isalpha(text[i])) {
+                for (int j = 0; j < multi; ++j) {
                     newString.push_back(text[i]);
+
+                    // Check if the length exceeds 50 characters
+                    if (newString.size() > 50) {
+                        return "TOO LONG";
+                    }
                 }
             }
 
-            // Reset multiplyer back to 1
-            multi = 1;
+            // Move to next character
+            ++i;
         }
+
         return newString;
     }
 };
