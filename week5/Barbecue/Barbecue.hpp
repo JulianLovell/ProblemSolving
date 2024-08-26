af76c5fcf520ecba35bfc65506ff5e8c7af77c37
JulianLovell <jlovell108@gmail.com>
2024-08-26 15:01:34 +0930

    Barbecue: Initial Implementation

--- week5/Barbecue/Barbecue.hpp
+++ week5/Barbecue/Barbecue.hpp
@@ -1,17 +1,70 @@
-#include <vector>
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
-using namespace std; 
+using namespace std;
 
-class Barbecue{
-    public:
+class Barbecue {
+   public:
+    int eliminate(int n, vector<int> voter, vector<int> excluded) {
 
-    int eliminate(int n, vector<int> voter, vector<int> excluded){
+        // Count votes recieved by each person
+        vector<int> votesAgainst(n, 0);
+        // Count how many times each person voted
+        vector<int> votesCast(n, 0);
 
-        // iterate through excluded and count frequency of each value
-        // if tied then
-            // iterate through voter and count frequency of each value
-            // return 
-        // otherwise return most frequent value
+        // Count votes against and votes cast
+        for (int i = 0; i < voter.size(); ++i) {
+            // Increase the count for the excluded person
+            votesAgainst[excluded[i]]++;
+
+            // Increase the count for the voter
+            votesCast[voter[i]]++;
+        }
+
+        // Find the person with the maximum votes against
+        int maxVotes = 0;
+
+        // List of candidates with the highest votes
+        vector<int> candidates;
+
+        for (int i = 0; i < n; ++i) {
+            if (votesAgainst[i] > maxVotes) {
+                maxVotes = votesAgainst[i];
+
+                // Clear the previous candidates
+                candidates.clear();
+                candidates.push_back(i);
+            } else if (votesAgainst[i] == maxVotes) {
+                // Add the person to candidates if they have the same votes
+                candidates.push_back(i);
+            }
+        }
+
+        // If there's only one candidate, return them
+        if (candidates.size() == 1) {
+            return candidates[0];
+        }
+
+        // If there's a tie, break it by finding who voted the most
+        int maxVotesCast = 0;
+        vector<int> finalCandidates;
+
+        for (int i = 0; i < candidates.size(); ++i) {
+            int candidate = candidates[i];
+            if (votesCast[candidate] > maxVotesCast) {
+                maxVotesCast = votesCast[candidate];
+                
+                // Clear previous candidates
+                finalCandidates.clear();
+                finalCandidates.push_back(candidate);
+            } else if (votesCast[candidate] == maxVotesCast) {
+                // Add the person to candidates if they have the same votes
+                finalCandidates.push_back(candidate);
+            }
+        }
+
+        // If there's still a tie, return the person with the smallest index
+        return *min_element(finalCandidates.begin(), finalCandidates.end());
     }
 };
